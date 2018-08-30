50f5acb0a504bab54375dd06eb31d90abf63a640
Chuck White <chhwhite2@gmail.com>
2018-08-30 14:25:04 -0500

    Read in many files.

--- gdp_plots.py
+++ gdp_plots.py
@@ -6,21 +6,22 @@ import matplotlib.pyplot as plt
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
-filename = sys.argv[1]
+file_list = sys.argv[1:]
 
-# read data into a pandas dataframe and transpose
-data = pandas.read_csv(filename, index_col = 'country').T
+for filename in file_list:
+	# read data into a pandas dataframe and transpose
+	data = pandas.read_csv(filename, index_col = 'country').T
 
-# create a plot the transposed data
-ax = data.plot(title=filename)
+	# create a plot the transposed data
+	ax = data.plot(title=filename)
 
-#axes labels
-ax.set_xlabel('Year')
-ax.set_ylabel('GDP per capita')
+	#axes labels
+	ax.set_xlabel('Year')
+	ax.set_ylabel('GDP per capita')
 
-#set axes ticks
-ax.set_xticks(range(len(data.index)))
-ax.set_xticklabels(data.index, rotation=45)
+	#set axes ticks
+	ax.set_xticks(range(len(data.index)))
+	ax.set_xticklabels(data.index, rotation=45)
 
-# display the plot
-plt.show()
+	# display the plot
+	plt.show()
