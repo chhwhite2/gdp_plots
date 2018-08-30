5f32f06645f423c6d7bcd97fb43a0558d4be8e94
Chuck White <chhwhite2@gmail.com>
2018-08-30 15:19:42 -0500

    added checks for flags and args

--- gdp_plots.py
+++ gdp_plots.py
@@ -1,29 +1,28 @@
 import pandas
 import sys
-# we need to import part of matplotlib
-# because we are no longer in a notebook
 import matplotlib.pyplot as plt
 import glob
 
-if sys.argv[1]=='-a':
+
+arguments = sys.argv
+
+if len(arguments)==1:
+	print('Usage: python gdp_plots <filename(s)> \n \
+	or use -a for all gdp data in directory')
+	exit()
+if arguments[1]=='-a':
 	file_list = glob.glob('*gdp*.csv')
+	if len(file_list)==0:
+		print('no files in cwd')
+		exit()
 else:
 	file_list = sys.argv[1:]
 
 for filename in file_list:
-	# read data into a pandas dataframe and transpose
 	data = pandas.read_csv(filename, index_col = 'country').T
-
-	# create a plot the transposed data
 	ax = data.plot(title=filename)
-
-	#axes labels
 	ax.set_xlabel('Year')
 	ax.set_ylabel('GDP per capita')
-
-	#set axes ticks
 	ax.set_xticks(range(len(data.index)))
 	ax.set_xticklabels(data.index, rotation=45)
-
-	# display the plot
 	plt.show()
