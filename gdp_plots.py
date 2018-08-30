349ce3518e9eb5bbe92eecf631cb8387ac8d246d
Chuck White <chhwhite2@gmail.com>
2018-08-30 13:48:35 -0500

    Fixed bug with filename and titles

--- gdp_plots.py
+++ gdp_plots.py
@@ -5,12 +5,21 @@ import matplotlib.pyplot as plt
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
+filename = 'gapminder_gdp_oceania.csv'
 
 # read data into a pandas dataframe and transpose
-data = pandas.read_csv('gapminder_gdp_oceania.csv', index_col = 'country').T
+data = pandas.read_csv(filename, index_col = 'country').T
 
 # create a plot the transposed data
-ax = data.plot()
+ax = data.plot(title=filename)
+
+#axes labels
+ax.set_xlabel('Year')
+ax.set_ylabel('GDP per capita')
+
+#set axes ticks
+ax.set_xticks(range(len(data.index)))
+ax.set_xticklabels(data.index, rotation=45)
 
 # display the plot
 plt.show()
